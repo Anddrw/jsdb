7df90fcfb1b179b04cd4a2fc03157b4d18fe3d93
Anddrw <andrei.vlad1598@gmail.com>
2019-01-25 22:26:18 +0200

    DataSource - Fix issue when starting jsdb while shard storage is missing

--- Jsdb/components/JsdbDataSource.cpp
+++ Jsdb/components/JsdbDataSource.cpp
@@ -28,6 +28,11 @@ bool DataSource::initDS()
 
 	LOG_INFO("Trying to regenerate shards from: " + targetDir.generic_string());
 
+	// Check if the shards storage path exists, if not create it
+	if (boost::filesystem::is_directory(targetDir.relative_path())) {
+		boost::filesystem::create_directory(targetDir.relative_path());
+	}
+
 	boost::filesystem::recursive_directory_iterator iter(targetDir), eod;
 
 	std::vector<std::string> shards;
